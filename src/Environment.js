6f82e00892146d35f8229da064270cfa5680218d
elrohil <w.stanek123@gmail.com>
2020-03-05 19:29:31 +0100

    Implemented environment emulator

--- src/Environment.js
+++ src/Environment.js
@@ -12,27 +12,39 @@ const COMMANDS = [
 const validateCommand = ({ command, args }) => {
   switch (command) {
     case 'PLACE_HEATER': {
-      const { id, temperature } = args;
+      const { id, temperature, x, y } = args;
       if (!id) {
         throw new Error('Missing parameter id');
       }
       if (!temperature) {
         throw new Error('Missing parameter temperature');
       }
+      if (!x) {
+        throw new Error('Missing parameter x');
+      }
+      if (!y) {
+        throw new Error('Missing parameter y');
+      }
       return;
     }
     case 'SET_TEMPERATURE':
-    case 'SET_PRESSURE':
     case 'SET_HUMIDITY': {
       const { value, from, to } = args;
-      if (!value) {
+      if (typeof value !== 'number') {
         throw new Error('Missing parameter value');
       }
-      if (!from || !from.x || from.y) {
+      if (!from || typeof from.x !== 'number' || typeof from.y !== 'number') {
         throw new Error('Missing parameter from');
       }
-      if (to && (!to.x || !to.y)) {
+      if (!to || typeof to.x !== 'number' || typeof to.y !== 'number') {
         throw new Error('Invalid parameter to');
+      }
+      return;
+    }
+    case 'SET_PRESSURE': {
+      const { value } = args;
+      if (!value) {
+        throw new Error('Missing parameter value');
       }
       return;
     }
@@ -49,19 +61,118 @@ const validateCommand = ({ command, args }) => {
 };
 
 
-const createEnvironment = () => {
+const createEnvironment = ({ epochDuration = 1000 } = {}) => {
   const actualState = new Array(WIDTH);
   let i = actualState.length;
   while (i--) {
     actualState[i] = new Array(HEIGHT);
     let j = actualState[i].length;
     while (j--) {
-      actualState[i][j] = [20, 1000, 40];
+      actualState[i][j] = [20, 40];
     }
   }
 
+  const state = {
+    epoch: 0,
+    executing: false,
+    pressure: 1000,
+    environment: actualState,
+    prevState: null,
+    heaters: [],
+  };
+
   const intervalQueue = [];
   const oneshotQueue = [];
+
+  const handleCommand = ([command, args]) => {
+    switch (command) {
+      case 'PLACE_HEATER': {
+        const { id, temperature, x, y } = args;
+        state.heaters.push({ id, temperature, x, y });
+        return;
+      }
+      case 'REMOVE_HEATER': {
+        const { id } = args;
+        const index = state.heaters.findIndex(heater => heater.id === id);
+        if (index !== -1) {
+          state.heaters.splice(index, 1);
+        }
+        return;
+      }
+      case 'SET_PRESSURE': {
+        const { value } = args;
+        state.pressure = value;
+        return;
+      }
+      case 'SET_HUMIDITY':
+      case 'SET_TEMPERATURE': {
+        const { value, from, to } = args;
+        const fromX = Math.min(from.x, to.x);
+        const fromY = Math.min(from.y, to.y);
+        const toX = Math.max(from.x, to.x);
+        const toY = Math.max(from.y, to.y);
+        const rows = state.environment
+          .slice(fromX, toX - fromX + 1)
+          .map(row => row.slice(fromY, toY - fromY + 1))
+          .flat();
+        const register = command === 'SET_TEMPERATURE' ? 0 : 1;
+        let i = rows.length;
+        while (i--) {
+          rows[i][register] = value;
+        }
+        return;
+      }
+      default:
+    }
+  };
+
+  const iterate = () => {
+    setTimeout(iterate, epochDuration);
+    state.epoch += 1;
+    state.prevState = [
+      ...state.environment
+        .map(row => [...row])
+    ];
+    console.log(`EPOCH: ${state.epoch} - ${Date.now()}`);
+    let w = WIDTH;
+    while (w--) {
+      let h = HEIGHT;
+      while (h--) {
+        const beginW = w ? w - 1 : w;
+        const endW = w + 2;
+        const beginH = h ? h - 1 : h;
+        const endH = h + 2;
+        const tile = state.prevState.slice(beginW, endW).map(s => s.slice(beginH, endH)).flat();
+        state.environment[w][h] = tile
+          .reduce(([tempSum, humSum], [temp, hum]) => [tempSum + temp, humSum + hum], [0, 0])
+          .map(v => v / tile.length);
+      }
+    }
+    let i = state.heaters.length;
+    while (i--) {
+      const { x, y, temperature } = state.heaters[i];
+      if (x >= 0 && x < WIDTH && y >= 0 && y < HEIGHT) {
+        state.environment[x][y][0] = Math.max(temperature, state.environment[x][y][0]);
+      }
+    }
+    let interval = intervalQueue.length;
+    while (interval--) {
+      if (state.epoch % interval === 0 && intervalQueue[interval]) {
+        const queue = intervalQueue[interval];
+        let i = queue.length;
+        while (i--) {
+          handleCommand(queue[i]);
+        }
+      }
+    }
+    if (oneshotQueue[state.epoch]) {
+      const queue = oneshotQueue[state.epoch];
+      let i = queue.length;
+      while (i--) {
+        handleCommand(queue[i]);
+      }
+    }
+  };
 
   return {
     every(iteration, { command, args }) {
@@ -70,7 +181,7 @@ const createEnvironment = () => {
       if (!intervalQueue[interval]) {
         intervalQueue[interval] = [];
       }
-      intervalQueue[interval].push([command, args]);
+      intervalQueue[interval].unshift([command, args]);
     },
     after(iteration, { command, args }) {
       validateCommand({ command, args });
@@ -78,7 +189,17 @@ const createEnvironment = () => {
       if (!oneshotQueue[interval]) {
         oneshotQueue[interval] = [];
       }
-      oneshotQueue[interval].push([command, args]);
+      oneshotQueue[interval].unshift([command, args]);
+    },
+    execute() {
+      if (!state.executing) {
+        state.executing = true;
+        iterate();
+      }
     }
   }
+};
+
+module.exports = {
+  createEnvironment,
 };
--- src/index.js
+++ src/index.js
@@ -1,2 +1,12 @@
+const { createEnvironment } = require('./Environment');
+
 const { MQTT_BROKER_URL } = process.env;
 
+const environment = createEnvironment({ epochDuration: 1000 });
+environment.after(10, { command: 'SET_TEMPERATURE', args: { value: 80, from: { x: 0, y: 0 }, to: { x: 2, y: 2 } } });
+environment.after(5, { command: 'SET_HUMIDITY', args: { value: 20, from: { x: 0, y: 2 }, to: { x: 2, y: 4 } } });
+environment.after(15, { command: 'PLACE_HEATER', args: { temperature: 100, x: 9, y: 9, id: 'heater' } });
+environment.after(100, { command: 'REMOVE_HEATER', args: { id: 'heater' } });
+environment.every(300, { command: 'SET_TEMPERATURE', args: { value: 0, from: { x: 0, y: 0 }, to: { x: 5, y: 5 } } });
+
+environment.execute();
